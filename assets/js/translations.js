98ac44597e6724024dfa54f90be65e7d33d0241f
Nathaniel Michael <natecmichael@gmail.com>
2018-03-15 15:54:24 -0700

    missed translation stuff

---
+++ assets/js/translations.js
@@ -0,0 +1,76 @@
+var i18n_en_dict = {
+  "t_nav_home": "Home",
+  "t_nav_video": "Video",
+  "t_nav_vision": "Vision",
+  "t_nav_whitepaper": "Whitepaper",
+  "t_nav_team": "Team",
+  "t_nav_contact": "Contact",
+  "t_xbv_token_sale": "XBV Token Sale",
+  "t_token_sales": "Token Sales",
+  "t_distributed": "Distributed",
+  "t_current_price": "Current Price",
+  "t_contributors": "Contributors",
+  "t_buy_now": "Buy XBV Now! @ ForkDelta",
+  "t_xbv_newsletter": "XBV Newsletter",
+  "t_sign_up_now": "Sign Up Now!",
+  "t_sign_up": "Sign Up!",
+  "t_vision": "Vision",
+  "t_vision_text": "BlockVentures assists organizations in the integration and execution of blockchain technology systems at the most critical application points up and down the organization's value chain. This blockchain integration introduces tremendous benefits related to transparency, security, and logistics efficiencies management functionality. BlockVentures serves multi-national corporate partners across a variety of industries, governmental agencies, and its own underlying business units in Latin America and North America. The nature of blockchain technology makes it nearly impossible for data to be corrupted and allows users to have a secure peer to peer relationship while maintaining data integrity. BlockVentures' use of decentralized applications written as smart contracts on the Ethereum blockchain, creates an ecosystem that generates demand for their ERC-223 XBV utility token.",
+  "t_whitepaper": "Whitepaper",
+  "t_download_whitepaper": "Download Whitepaper",
+  "t_our_team": "Our Team",
+
+  "t_albert_rosario_bio": "Al Rosario is an internationally recognized entrepreneur and New York native. Al brings over 35 years of expertise in strategic planning, tactical execution, and business operations leadership. Recognized as a leader in results-driven management with vast experience in marketing and distribution, he brings key relationships across a wide variety of industries to the company.",
+  "t_albert_rosario_title": "CEO / Founder",
+  "t_scott_magers_bio": "Scott Magers is an entrepreneur, international businessman and a private investor. Scott has developed over 50 products in the wholesale/retail trading sphere, with experience in online and offline business since 1998. In 2001, he created a top-selling health beverage with distribution in over 40 countries worldwide.",
+  "t_scott_magers_title": "Executive VP / Co-Founder",
+  "t_martin_mendoza_bio": "Martin Mendoza graduated with a PhD in Law from the International Institute of Law and the State & a Master’s in Criminal Law from the University of Guadalajara Faculty of Law. He currently serves on the National Federation of Associations of Lawyers, A.C. as well as the Association of Lawers in Acapulco, A.C.",
+  "t_martin_mendoza_title": "Chief Legal Officer",
+  "t_john_eisberg_bio": "John Eisberg has over 22 years of experience in development, programming and management of multiple IT projects worldwide. From Beijing, China to Seattle, Washington, John has built cloud servers, managed software architecture teams and has written mobile apps. John heads up the BlockVentures Blockchain development team.",
+  "t_john_eisberg_title": "CTO",
+  "t_dr_sofia_camargo_bio": "Sofia has over 20 years of experience in health care. She holds numerous diplomas (including Master's in education). She has worked as an ER doctor & a secretariat of National Defense, and also serves as the President of the Bioethics Committee. She worked at the military hospital in Jalisco for 21 years.",
+  "t_dr_sofia_camargo_title": "Director of Government Affairs",
+  "t_jose_miguel_rivas_bio": "Jose comes from an sport backround. He played about 10 years of profesional baseball. He also has over 5 years of experience in managaing operations. He holds a bachellors of science in International Business and a minor in Economics.",
+  "t_jose_miguel_rivas_title": "Director of Operations, Mexico",
+  "t_jeff_bolding_bio": "Jeff Bolding is a IT Project Manager, with 18 + years experience implementing Non-Cloud and Cloud based Infor/Lawson ERP Software for mid size to large companies. He is also a Multi-Talented Programmer and SQL database experienced. Prior, to his IT experience, for 10+ years Jeff managed Accounting Departments in mid to large size companies. Jeff has a BBA degree majoring in Accounting and Finance from the University of Wisconsin, he is a state of Wisconsin CPA and earned Microsoft MCSE and MCSD Certifications.",
+  "t_jeff_bolding_title": "IT Project Manager",
+  "t_leon_martinez_bio": "With a long and extensive experience, Leon is an expert in administration and business development. He holds a Master degree in Management & Generation of Innovation from the University of Cambridge, where he also recieved a diploma for Strategic Innovation. Leon is also certified by ISACA COBIT",
+  "t_leon_martinez_title": "Director, Sales & Business Administration",
+  "t_jose_luis_cisneros_bio": "Jose holds a degree in Business Administration from Vocational Institute Enrique Díaz de León, and has a diverse portfolio of experience, working as a teacher at the Free College of University Studies, as a Credit & Collections manager, as well as an Administrator & Real Estate Advisor.",
+  "t_jose_luis_cisneros_title": "Director of Administration, Mexico",
+  "t_brenda_martinez_bio": "Brenda is an Administrative Manager, Accountant and Auditor with a wide experience in those areas. She develops financial policies, implements control processes, maintains and deploys certifications, as well as the audit area.",
+  "t_brenda_martinez_title": "CFO / CPA",
+  "t_robert_oleary_bio": "Mr. O'Leary has created, grown and sold multiple businesses over his 25 year career. He has also provided sales and strategic consulting and built managed services portfolios for a range of companies. Mr. O'Leary supports business, partnership and revenue generation efforts and evaluations for both BlockVentures and BVC. Mr. O'Leary earned a BS in Mechanical Engineering from Bucknell University, and an MBA in Finance and Entrepreneurship from the University of Southern California.",
+  "t_robert_oleary_title": "Business Development",
+  "t_tim_ellis_bio": "Tim Ellis Since earning his Masters Degree from Azusa Pacific University, Tim has been branding and producing meaningful content for over 30 years. Tim is the founding partner of Rivet Design and Production serving clients including the FOX networks, ABC, NBC, PBS, Warner Bros., MGM, and National Geographic. Tim has also produced for advertising clients such as Saatchi & Saatchi, Young & Rubicam, Deutsche, FCB and Chiat/Day. Additionally, he has created and produced live experiences for venues from clubs to arenas to stadiums.",
+  "t_tim_ellis_title": "Brand Management",
+
+  "t_advisors": "Advisors",
+
+  "t_dr_brom_mahbod_bio": "Dr. Brom Mahbod, after receiving his Ph.D. degree in Electrical and Computer Engineering, joined the research team at Hewlett Packard Laboratories in Palo Alto, CA, working on the first Object Oriented Database. The effort resulted in the first commercially available Object Oriented Database (OODB) from HP several years later.",
+  "t_dr_brom_mahbod_title": "Technolgy Advisor",
+  "t_dean_anastos_bio": "Dean Anastos is a software engineer. Dean found that software development was the niche for him. With years of experience working with both corporate level and local business clients, Dean is looking to continually extend his programming portfolio to companies looking to grow in the online marketing world.",
+  "t_dean_anastos_title": "Blockchain Advisor",
+  "t_jit_singh_bio": "Jit Singh founded Business Technology Services in early 2000. His background in technology extends well into his youth when his dad first bought him a Commodore 64. If there is a problem related to IT, Jagdeash will find the solution. His certifications in Cisco and Microsoft bring great value to our company.",
+  "t_jit_singh_title": "Security & Database Technology",
+  "t_bob_terry_bio": "Mr. Terry is a member of the firm’s Regulatory Practice Group, advising and representing broker-dealers, investment advisors and individuals in regulatory, compliance, and enforcement. He also provides advice and representation to clients in connection with other corporate matters, including raising capital.",
+  "t_bob_terry_title": "U.S. Legal Advisor",
+
+  "t_contact_us": "Contact Us",
+  "t_send_message": "Send Message",
+  "t_copyright": "Copyright © 2018 BlockVenture El Sol No. 2943 Col. Jardines del Bosque 44520 Guadalajara, Jalisco, Mexico. All Rights Reserved."
+};
+
+var i18n_es_dict = {};
+
+var i18n_ru_dict = {};
+
+var i18n_xbv = {
+  translate: function() {
+    for (var key in i18n_en_dict) {
+      if (i18n_en_dict.hasOwnProperty(key)) {
+        $('#'+key)._t(key);
+      }
+    }
+  }
+};
